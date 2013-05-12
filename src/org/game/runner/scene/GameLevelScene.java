2f918af1b449da37e011661228192ca588179185
Karl Woditsch <k.woditsch@gmail.com>
2013-05-12 17:35:01 +0200

    Add physics base

--- src/org/game/runner/scene/GameLevelScene.java
+++ src/org/game/runner/scene/GameLevelScene.java
@@ -5,9 +5,17 @@
 package org.game.runner.scene;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.extension.physics.box2d.FixedStepPhysicsWorld;
+import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
 import org.andengine.input.touch.TouchEvent;
 import org.andengine.util.adt.color.Color;
@@ -22,12 +30,31 @@ import org.game.runner.manager.SceneManager.SceneType;
  */
 public class GameLevelScene extends BaseScene implements IOnSceneTouchListener{
     private PhysicsWorld physicWorld;
+    private Body groundBody;
+    private Body testBody;
     
     @Override
     public void createScene() {
-        this.physicWorld = new PhysicsWorld(new Vector2(0, -1), false);
-        
         this.setBackground(new Background(Color.BLACK));
+        
+        this.physicWorld = new FixedStepPhysicsWorld(30, new Vector2(0, -20), false);
+        this.registerUpdateHandler(this.physicWorld);
+        
+        final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0, 0, 0);
+        
+        Rectangle ground = new Rectangle(400, 50, 800, 10, this.vbom);
+	this.groundBody = PhysicsFactory.createBoxBody(this.physicWorld, ground, BodyDef.BodyType.StaticBody, wallFixtureDef);
+        
+        Sprite test = new Sprite(400, 200, this.resourcesManager.testGamePlayer, this.vbom);
+	this.testBody = PhysicsFactory.createBoxBody(this.physicWorld, test, BodyDef.BodyType.DynamicBody, wallFixtureDef);
+        test.setUserData(this.testBody);
+        
+        attachChild(ground);
+        
+        attachChild(test);
+        this.physicWorld.registerPhysicsConnector(new PhysicsConnector(test, this.testBody, true, false));
+        
+        this.setOnSceneTouchListener(this);
     }
 
     @Override
@@ -56,6 +83,9 @@ public class GameLevelScene extends BaseScene implements IOnSceneTouchListener{
     }
 
     public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+        if (pSceneTouchEvent.isActionDown()){
+            this.testBody.setLinearVelocity(new Vector2(0, 10));
+        }
         return false;
     }
     
