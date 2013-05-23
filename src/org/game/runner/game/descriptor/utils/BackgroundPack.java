048b1c9af0c3cf92c39b4a444e20d7783c1d8087
Karl Woditsch <k.woditsch@gmail.com>
2013-05-23 18:41:09 +0200

    Implement hill backgrounds

--- src/org/game/runner/game/descriptor/utils/BackgroundPack.java
+++ src/org/game/runner/game/descriptor/utils/BackgroundPack.java
@@ -12,7 +12,8 @@ public enum BackgroundPack{
     MOUNTAIN(Layer.CLOUDS_2.withSpeed(20), Layer.CLOUDS_1.withSpeed(25), Layer.MOUNTAIN_2.withSpeed(30), Layer.MOUNTAIN_1.withSpeed(35)), 
     FOREST(Layer.CLOUDS_2.withSpeed(20), Layer.CLOUDS_1.withSpeed(25), Layer.FOREST_2.withSpeed(30), Layer.FOREST_1.withSpeed(35)), 
     CITY(Layer.CLOUDS_2.withSpeed(20), Layer.CLOUDS_1.withSpeed(25), Layer.CITY_2.withSpeed(30), Layer.CITY_1.withSpeed(35)), 
-    DESERT(Layer.CLOUDS_2.withSpeed(20), Layer.CLOUDS_1.withSpeed(25), Layer.DESERT_2.withSpeed(30), Layer.DESERT_1.withSpeed(35));
+    DESERT(Layer.CLOUDS_2.withSpeed(20), Layer.CLOUDS_1.withSpeed(25), Layer.DESERT_2.withSpeed(30), Layer.DESERT_1.withSpeed(35)), 
+    HILL(Layer.CLOUDS_2.withSpeed(20), Layer.CLOUDS_1.withSpeed(25), Layer.DESERT_2.withSpeed(30), Layer.DESERT_1.withSpeed(35));
 
     public enum Layer{
         CLOUDS_1(0, 220, "clouds_1"),
@@ -24,7 +25,9 @@ public enum BackgroundPack{
         MOUNTAIN_1(0, 0, "mountain_1"),
         MOUNTAIN_2(0, 0, "mountain_2"),
         FOREST_1(0, 0, "forest_1"),
-        FOREST_2(0, 0, "forest_2");
+        FOREST_2(0, 0, "forest_2"),
+        HILL_1(0, 0, "hill_1"),
+        HILL_2(0, 0, "hill_2");
 
         public float x;
         public float y;
