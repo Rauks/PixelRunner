8a60951e8ff02c39ae89b3e316d61861ef95d672
Karl Woditsch <k.woditsch@gmail.com>
2013-05-25 14:30:06 +0200

    Add resume/pause listener

--- src/org/game/runner/scene/LoadingScene.java
+++ src/org/game/runner/scene/LoadingScene.java
@@ -16,13 +16,27 @@ import org.game.runner.manager.SceneManager.SceneType;
  * @author Karl
  */
 public class LoadingScene extends BaseScene{
+    public static abstract class LoadingListener{
+        public abstract void onPause();
+        public abstract void onResume();
+    }
+    
     private Text title;
+    private LoadingListener listener;
     
     @Override
     public void createScene() {
         this.setBackground(new Background(Color.BLACK));
         this.title = new Text(GameActivity.CAMERA_WIDTH/2, GameActivity.CAMERA_HEIGHT/2, resourcesManager.fontPixel_60, "LOADING...", vbom);
         attachChild(this.title);
+    }
+    
+    public void setLoadingListener(LoadingListener listener){
+        this.listener = listener;
+        
+    }
+    public void removeLoadingListener(){
+        this.listener = null;
     }
 
     @Override
@@ -36,10 +50,16 @@ public class LoadingScene extends BaseScene{
 
     @Override
     public void onPause() {
+        if(this.listener != null){
+            this.listener.onPause();
+        }
     }
 
     @Override
     public void onResume() {
+        if(this.listener != null){
+            this.listener.onResume();
+        }
     }
 
     @Override
