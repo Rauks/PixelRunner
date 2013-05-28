dc69cbede8e6e5d1107faafc1f769761afd4dd75
Karl Woditsch <k.woditsch@gmail.com>
2013-05-28 17:58:23 +0200

    Fix training levels references

--- src/org/game/runner/scene/base/element/PageTutorial.java
+++ src/org/game/runner/scene/base/element/PageTutorial.java
@@ -14,9 +14,9 @@ import org.game.runner.manager.ResourcesManager;
 public class PageTutorial extends Page{
     public PageTutorial(final float pWidth, final float pHeight, int worldId, final VertexBufferObjectManager pVertexBufferObjectManager){
         super(pWidth, pHeight, worldId, 0, pVertexBufferObjectManager);
-        this.addElement(new PageElement(0, 0, 0, false, ResourcesManager.getInstance().lvlJump, pVertexBufferObjectManager));
-        this.addElement(new PageElement(0, 0, 1, false, ResourcesManager.getInstance().lvlRoll, pVertexBufferObjectManager));
-        this.addElement(new PageElement(0, 0, 2, false, ResourcesManager.getInstance().lvlDoubleJump, pVertexBufferObjectManager));
+        this.addElement(new PageElement(0, 0, 1, false, ResourcesManager.getInstance().lvlJump, pVertexBufferObjectManager));
+        this.addElement(new PageElement(0, 0, 2, false, ResourcesManager.getInstance().lvlRoll, pVertexBufferObjectManager));
+        this.addElement(new PageElement(0, 0, 3, false, ResourcesManager.getInstance().lvlDoubleJump, pVertexBufferObjectManager));
         this.addElement(new PageElement(0, 0, 4, false, ResourcesManager.getInstance().lvlPlatform, pVertexBufferObjectManager));
         this.setProgress(4);
     }
