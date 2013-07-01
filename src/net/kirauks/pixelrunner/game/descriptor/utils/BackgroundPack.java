843d8a977b88214a6f3fc75444bb7261338cfc41
Karl Woditsch <k.woditsch@gmail.com>
2013-07-01 14:48:30 +0200

    Add background #8

--- src/net/kirauks/pixelrunner/game/descriptor/utils/BackgroundPack.java
+++ src/net/kirauks/pixelrunner/game/descriptor/utils/BackgroundPack.java
@@ -17,6 +17,7 @@ public enum BackgroundPack{
     DESERT(Layer.CLOUDS_2.withSpeed(20), Layer.CLOUDS_1.withSpeed(25), Layer.DESERT_2.withSpeed(30), Layer.DESERT_1.withSpeed(35)), 
     HILLS(Layer.CLOUDS_2.withSpeed(20), Layer.CLOUDS_1.withSpeed(25), Layer.HILL_2.withSpeed(30), Layer.HILL_1.withSpeed(35)), 
     SWEETS(Layer.SWEET_4.withSpeed(20), Layer.SWEET_3.withSpeed(25), Layer.SWEET_2.withSpeed(30), Layer.SWEET_1.withSpeed(35)), 
+    SPACE(Layer.STARS_2.withSpeed(20), Layer.STARS_1.withSpeed(25), Layer.PLANETS_2.withSpeed(30), Layer.PLANETS_1.withSpeed(35)),
     TRAINING_JUMP(Layer.JUMP_1.withSpeed(25), Layer.JUMP_2.withSpeed(30)), 
     TRAINING_ROLL(Layer.ROLL_1.withSpeed(25), Layer.ROLL_2.withSpeed(30)), 
     TRAINING_DOUBLEJUMP(Layer.DOUBLEJUMP_1.withSpeed(25), Layer.DOUBLEJUMP_2.withSpeed(30)), 
@@ -58,14 +59,16 @@ public enum BackgroundPack{
                 return BackgroundPack.FOREST;
             case HILLS:
                 return BackgroundPack.HILLS;
-            default:
             case SWEETS:
                 return BackgroundPack.SWEETS;
+            default:
+            case SPACE:
+                return BackgroundPack.SPACE;
         }
     }
     
     public static BackgroundPack getRamdomBackgroundPack(){
-        switch(RANDGEN.nextInt(6)){
+        switch(RANDGEN.nextInt(7)){
             case 0:
                 return BackgroundPack.MOUNTAIN;
             case 1:
@@ -76,9 +79,11 @@ public enum BackgroundPack{
                 return BackgroundPack.FOREST;
             case 4:
                 return BackgroundPack.HILLS;
-            default:
             case 5:
                 return BackgroundPack.SWEETS;
+            default:
+            case 6:
+                return BackgroundPack.SPACE;
         }
     }
 }
